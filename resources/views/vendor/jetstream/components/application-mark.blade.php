fc14a1112bdc73554241fbc8bae75b708d904b6c
oliver <dopplereeffect1992@gmail.com>
2021-03-24 04:21:25 +0100

    finally  working sample

---
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -0,0 +1,1 @@
+<img src="https://www.connectyu.com/themes/wondertag/img/icon.png" class="h-10 w-10 rounded-fullalt " alt="">
---
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -0,0 +1,7 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+
+
+    <div class="mt-6 text-gray-500">
+        <livewire:users/>
+    </div>
+</div>
